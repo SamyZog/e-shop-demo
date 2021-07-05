6d0b42a2fa7faa36882806ef69ebbaa491d5986f
Samy Zogeyb <samyzogeyb@gmail.com>
2021-07-05 14:26:03 +0400

    replaced BrowserRouter with HashRouter for deplyment on github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import CartProvider from "./context/CartProvider";
 import DataProvider from "./context/DataProvider";
